0080034e55aef3847b0d26c663d389bc93c62467
raelvanz <abdisembada567@gmail.com>
2025-09-04 08:00:24 +0700

    [FE] Calc total on icon notification

--- flutter_rpg/lib/shared/cart_icon.dart
+++ flutter_rpg/lib/shared/cart_icon.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_rpg/screens/product/cart_screen.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:flutter_rpg/providers/cart_provider.dart';
 
-class CartIcon extends StatefulWidget {
+class CartIcon extends ConsumerWidget {
   const CartIcon({super.key});
 
   @override
-  State<CartIcon> createState() => _CartIconState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cartCount = ref.watch(cartNotifierProvider).length;
 
-class _CartIconState extends State<CartIcon> {
-  @override
-  Widget build(BuildContext context) {
     return Stack(
       children: [
         IconButton(
@@ -23,18 +22,29 @@ class _CartIconState extends State<CartIcon> {
           },
           icon: Icon(Icons.shopping_bag_outlined, color: AppColors.textColor),
         ),
-        Positioned(
-          top: 6,
-          left: 10,
-          child: Container(
-            width: 12,
-            height: 12,
-            decoration: BoxDecoration(
-              color: AppColors.primaryColor,
-              borderRadius: BorderRadius.circular(9),
+        if (cartCount > 0)
+          Positioned(
+            top: 6,
+            left: 10,
+            child: Container(
+              width: 16,
+              height: 16,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(9),
+              ),
+              child: Center(
+                child: Text(
+                  '$cartCount',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
       ],
     );
   }
